c69c11e0166ca6b14215ae60b192b9f268da67b8
smulsasha@gmail.com <118166416+AlexHunter0209@users.noreply.github.com>
2023-04-06 00:24:43 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,11 +9,11 @@ const gallery = document.querySelector(".gallery");
 const galleryMarkup = galleryItems
   .map(({ preview, original, description }) => {
     return `<li>
-        <a class="gallery__item" href="${original}">
-            <img class="gallery__image"
-            src="${preview}" 
-            alt="${description}" />
-        </a>
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image"
+    src="${preview}" 
+    alt="${description}" />
+    </a>
     </li>`;
   })
   .join("");
