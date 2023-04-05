180ec3ad3379fafc350c4c27f30c38f33a6234a9
smulsasha@gmail.com <118166416+AlexHunter0209@users.noreply.github.com>
2023-04-06 00:19:03 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -10,13 +10,13 @@ const galleryMarkup = galleryItems
   .map(({ preview, original, description }) => {
     return `
     <div class = "gallery__item">
-    <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"/>
-    </a>
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"/>
+        </a>
     </div>
     `;
   })
